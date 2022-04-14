4059e9c4f7c68d1280222a743280567d7a371b07
Dominykas-Zernys <zernysdominykas@gmail.com>
2022-04-14 18:20:35 +0300

    empty pages created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Add from './pages/Add/Add';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Register from './pages/Register/Register';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Switch>
+        <Route path='/register'>
+          <Register />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/add'>
+          <Add />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
